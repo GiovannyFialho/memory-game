43c830b3c277c1111a5ffc78cfbe18a506279f9e
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2026-03-31 17:53:54 -0300

    setting auth store

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,13 @@
 import { Redirect } from "expo-router";
 
+import { useAuthStore } from "@/shared/stores/auth.store";
+
 export default function Index() {
+  const { user } = useAuthStore();
+
+  if (user) {
+    return <Redirect href="/(private)/home" />;
+  }
+
   return <Redirect href="/(public)/sign-in" />;
 }
